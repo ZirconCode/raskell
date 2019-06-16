bfb65c15908b683cdf1f5a800f85de6bacde1d81
zirconcode <gruenings@gmail.com>
2019-06-16 12:40:59 +0200

    slow progress

--- main.hs
+++ main.hs
@@ -1,13 +1,28 @@
 import UI.NCurses
+
+
 main :: IO ()
 main = runCurses $ do
     setEcho False
     w <- defaultWindow
+
+    x <- newColorID ColorWhite ColorBlue 5
+    
+    -- fmap setColor x 
+
     updateWindow w $ do
+        clear
+        setColor defaultColorID
         moveCursor 1 10
         drawString "Hello world!"
         moveCursor 3 10
         drawString "(press q to quit)"
+        moveCursor 20 20
+        
+        setColor x--(newColorID ColorBlue 5)
+        -- (newColorID ColorBlue ColorBlue 5)
+        -- drawString "testaaaaaaaaaaaaaaaaaaa"
+        drawString "~~~~~~~"
         moveCursor 0 0
     render
     waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
--- readme.txt
+++ readme.txt
@@ -1,1 +1,31 @@
+
+zurihac19 
+
+
 cabal install ncurses
+
+
+
+notes:
+https://hackage.haskell.org/package/brick
+- thanks yuri
+
+
+
+---
+
+notes: https://stackoverflow.com/questions/28624408/equal-vs-left-arrow-symbols-in-haskell
+
+do
+    x <- y
+    f x
+
+is equivalent to:
+
+y >>= \x -> f x
+
+- thanks siddartha
+
+
+
+
